3285c39a55e0f0e20f4a217ba6c13fffe2ca6a36
Zoe Ames <zoeames@gmail.com>
2018-07-07 11:22:40 -0500

    added inventory to private route

--- src/app/App.js
+++ src/app/App.js
@@ -4,7 +4,7 @@ import './App.css';
 
 // import Fish from '../components/Fish/Fish';
 import Home from '../components/Home/Home';
-// import Inventory from '../components/Inventory/Inventory';
+import Inventory from '../components/Inventory/Inventory';
 // import Login from '../components/Login/Login';
 import Navbar from '../components/Navbar/Navbar';
 // import New from '../components/New/New';
@@ -13,7 +13,27 @@ import Navbar from '../components/Navbar/Navbar';
 // import Register from '../components/Register/Register';
 // import SingleOrder from '../components/SingleOrder/SingleOrder';
 
+const PrivateRoute = ({ component: Component, authed, ...rest}) => {
+  return (
+    <Route
+      {...rest}
+      render={props =>
+        authed === true ? (
+          <Component {...props} />
+        ) : (
+          <Redirect
+            to={{ pathname: '/login', state: {from: props.location}}}
+          />
+        )
+      }
+    />
+  );
+};
+
 class App extends React.Component {
+  state={
+    authed: false,
+  }
   render () {
     return (
       <div className="App">
@@ -24,6 +44,11 @@ class App extends React.Component {
               <div className="row">
                 <Switch>
                   <Route path="/" exact component={Home}/>
+                  <PrivateRoute
+                    path="/inventory"
+                    authed={this.state.authed}
+                    component={Inventory}
+                  />
                 </Switch>
               </div>
             </div>
