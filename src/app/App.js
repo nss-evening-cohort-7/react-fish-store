d9616da3fa722283fa825f8665e7b4a1f7dd1466
Zoe Ames <zoeames@gmail.com>
2018-07-07 10:52:33 -0500

    added basic routing

--- src/app/App.js
+++ src/app/App.js
@@ -1,31 +1,34 @@
 import React from 'react';
+import {Route, BrowserRouter, Redirect, Switch}  from 'react-router-dom';
 import './App.css';
 
-import Fish from '../components/Fish/Fish';
+// import Fish from '../components/Fish/Fish';
 import Home from '../components/Home/Home';
-import Inventory from '../components/Inventory/Inventory';
-import Login from '../components/Login/Login';
+// import Inventory from '../components/Inventory/Inventory';
+// import Login from '../components/Login/Login';
 import Navbar from '../components/Navbar/Navbar';
-import New from '../components/New/New';
-import Order from '../components/Order/Order';
-import OrderSpa from '../components/OrderSpa/OrderSpa';
-import Register from '../components/Register/Register';
-import SingleOrder from '../components/SingleOrder/SingleOrder';
+// import New from '../components/New/New';
+// import Order from '../components/Order/Order';
+// import OrderSpa from '../components/OrderSpa/OrderSpa';
+// import Register from '../components/Register/Register';
+// import SingleOrder from '../components/SingleOrder/SingleOrder';
 
 class App extends React.Component {
   render () {
     return (
       <div className="App">
-        <Fish />
-        <Home />
-        <Inventory />
-        <Login />
-        <Navbar />
-        <New />
-        <Order />
-        <OrderSpa />
-        <Register />
-        <SingleOrder />
+        <BrowserRouter>
+          <div>
+            <Navbar />
+            <div className="container">
+              <div className="row">
+                <Switch>
+                  <Route path="/" exact component={Home}/>
+                </Switch>
+              </div>
+            </div>
+          </div>
+        </BrowserRouter>
       </div>
     );
   }
--- src/components/Home/Home.js
+++ src/components/Home/Home.js
@@ -6,7 +6,23 @@ class Home extends React.Component {
   render () {
     return (
       <div className="Home">
-        <h1>Home</h1>
+        <div className="catch-of-the-day">
+          <div className="menu">
+            <header className="top">
+              <h1>
+                Catch
+                <span className="ofThe">
+                  <span className="of">Of</span>
+                  <span className="the">The</span>
+                </span>
+                Day
+              </h1>
+              <h3 className="tagline">
+                <span>Fresh Seafood Market</span>
+              </h3>
+            </header>
+          </div>
+        </div>
       </div>
     );
   }
