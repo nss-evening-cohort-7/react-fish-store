0928ed63333c6cbf97f76ec2ba94178c947f1ab6
Zoe Ames <zoeames@gmail.com>
2018-07-07 13:25:53 -0500

    initial navbar

--- src/components/Navbar/Navbar.js
+++ src/components/Navbar/Navbar.js
@@ -1,4 +1,5 @@
 import React from 'react';
+import {Link} from 'react-router-dom';
 
 import './Navbar.css';
 
@@ -6,7 +7,26 @@ class Navbar extends React.Component {
   render () {
     return (
       <div className="Navbar">
-        <h1>Navbar</h1>
+        <nav className="navbar navbar-inverse">
+          <div className="container-fluid">
+            <div className="navbar-header">
+              <button type="button" className="navbar-toggle collapsed" data-toggle="collapse" data-target="#bs-example-navbar-collapse-1" aria-expanded="false">
+                <span className="sr-only">Toggle navigation</span>
+                <span className="icon-bar"></span>
+                <span className="icon-bar"></span>
+                <span className="icon-bar"></span>
+              </button>
+              <Link  to="/" className="navbar-brand">Fish Store</Link>
+            </div>
+            <div className="collapse navbar-collapse" id="bs-example-navbar-collapse-1">
+              <ul className="nav navbar-nav navbar-right">
+                <li>
+                  <Link to="/login">Login</Link>
+                </li>
+              </ul>
+            </div>
+          </div>
+        </nav>
       </div>
     );
   }
