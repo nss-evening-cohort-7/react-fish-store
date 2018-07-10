9c762f37d990aad3d58cec2f826ccef2d6860b24
Zoe Ames <zoeames@gmail.com>
2018-07-10 18:25:32 -0500

    wip

--- src/components/New/New.js
+++ src/components/New/New.js
@@ -48,7 +48,10 @@ class New extends React.Component {
             {fishComponents}
           </ul>
         </div>
-        <Order />
+        <Order
+          fishes={this.state.fishes}
+          order={this.state.order}
+        />
       </div>
     );
   }
--- src/components/Order/Order.js
+++ src/components/Order/Order.js
@@ -3,10 +3,27 @@ import React from 'react';
 import './Order.css';
 
 class Order extends React.Component {
+
+  renderOrder = (key) => {
+    const fish = this.props.fishes.find(x => x.id === key);
+    const count = this.props.order[key];
+
+    return (
+      <li>
+        {count} lbs {fish.name}
+      </li>
+    );
+  }
   render () {
+    const orderIds = Object.keys(this.props.order);
     return (
       <div className="Order">
-        <h1>Order</h1>
+        <h2>Order</h2>
+        <ul>
+          {orderIds.map(this.renderOrder)}
+        </ul>
+        <div className="total">Total:</div>
+        <button className="btn btn-default">Save Order</button>
       </div>
     );
   }
