36b174b73384cb1ce2272ea58f573d98310ac06f
Zoe Ames <zoeames@gmail.com>
2018-07-09 19:47:08 -0500

    inventory done

--- src/components/Fish/Fish.js
+++ src/components/Fish/Fish.js
@@ -1,13 +1,28 @@
 import React from 'react';
 
+import formatPrice from '../../helpers';
+
 import './Fish.css';
 
 class Fish extends React.Component {
   render () {
+    const { details } = this.props;
+    const isAvailable = details.status  === 'available';
+    const image = require(`${details.image}`);
     return (
-      <div className="Fish">
-        <h1>FISH</h1>
-      </div>
+      <li className="Fish">
+        <img src={image} alt={details.name}/>
+        <h3 className="name">
+          {details.name}
+          <span className="price">{formatPrice(details.price)}</span>
+        </h3>
+        <p>{details.desc}</p>
+        <button
+          disabled={!isAvailable}
+        >
+          {isAvailable ? 'Add To Order' : 'Sold Out!'}
+        </button>
+      </li>
     );
   }
 }
--- src/components/Inventory/Inventory.js
+++ src/components/Inventory/Inventory.js
@@ -1,6 +1,7 @@
 import React from 'react';
 
 import fishRequests from '../../firebaseRequests/fishes';
+import Fish from '../Fish/Fish';
 
 import './Inventory.css';
 
@@ -23,11 +24,14 @@ class Inventory extends React.Component {
   render () {
     const fishComponents = this.state.fishes.map((fish) => {
       return (
-        <h2>{fish.name}</h2>
+        <Fish
+          key={fish.id}
+          details={fish}
+        />
       );
     });
     return (
-      <div className="Inventory">
+      <div className="Inventory col-xs-12">
         <h1>Inventory</h1>
         <ul className="fishes">
           {fishComponents}
