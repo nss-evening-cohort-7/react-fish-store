010a6939a680213be988314776588ae1de06a965
Zoe Ames <zoeames@gmail.com>
2018-07-09 21:15:15 -0500

    inventory column on new page

--- src/components/New/New.js
+++ src/components/New/New.js
@@ -1,12 +1,47 @@
 import React from 'react';
 
+import Fish from '../Fish/Fish';
+import Order from '../Order/Order';
+
+import fishRequests from '../../firebaseRequests/fishes';
+
 import './New.css';
 
 class New extends React.Component {
+  state = {
+    fishes: [],
+    order: {},
+  };
+
+  componentDidMount () {
+    fishRequests
+      .getRequest()
+      .then((fishes) => {
+        this.setState({fishes});
+      })
+      .catch((err) => {
+        console.error('error with fish get request', err);
+      });
+  }
+
   render () {
+    const fishComponents = this.state.fishes.map((fish) => {
+      return (
+        <Fish
+          key={fish.id}
+          details={fish}
+        />
+      );
+    });
     return (
       <div className="New">
-        <h1>New</h1>
+        <div className="col-xs-8 inventory-container">
+          <h2>Inventory</h2>
+          <ul className="fishes">
+            {fishComponents}
+          </ul>
+        </div>
+        <Order />
       </div>
     );
   }
