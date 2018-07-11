b9b833db0691666e557f544ef31596eef9e4144e
Zoe Ames <zoeames@gmail.com>
2018-07-10 20:23:22 -0500

    delete

--- src/components/SingleOrder/SingleOrder.js
+++ src/components/SingleOrder/SingleOrder.js
@@ -2,11 +2,26 @@ import React from 'react';
 
 import './SingleOrder.css';
 
+import orderRequests from '../../firebaseRequests/orders';
 class SingleOrder extends React.Component {
+
+  deleteOrderClick = () => {
+    const firebaseId = this.props.match.params.id;
+    orderRequests
+      .deleteRequest(firebaseId)
+      .then(() => {
+        this.props.history.push('/orders');
+      })
+      .catch(((err) => {
+        console.error('error with get delete request', err);
+      }));
+  }
+
   render () {
     return (
       <div className="SingleOrder">
         <h1>SingleOrder</h1>
+        <button className="btn btn-danger" onClick={this.deleteOrderClick}>Delete Order</button>
       </div>
     );
   }
